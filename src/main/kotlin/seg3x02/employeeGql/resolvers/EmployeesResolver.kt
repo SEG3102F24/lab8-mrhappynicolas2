e75285b978a5f8514f6ebd15a4042f13ba1b193e
Nicolas Bérubé <nberu056@uottawa.ca>
2024-11-03 10:51:41 -0500

    fixed

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -23,39 +23,18 @@ class EmployeesResolver(private val employeesRepository: EmployeesRepository) {
     }
 
     @MutationMapping
-    fun addEmployee(@Argument("createEmployeeInput") input: CreateEmployeeInput): Employee {
+    fun newEmployee(@Argument("createEmployeeInput") input: CreateEmployeeInput): Employee {
         if(input.name != null && input.dateOfBirth != null && input.city != null && input.salary != null) {
             val employee = Employee(
-                name = input.name,
-                dateOfBirth = input.dateOfBirth,
-                city = input.city,
-                salary = input.salary,
-                gender = input.gender,
-                email = input.email)
+                input.name,
+                input.dateOfBirth,
+                input.city,
+                input.salary,
+                input.gender,
+                input.email)
             employee.id = UUID.randomUUID().toString()
             employeesRepository.save(employee)
             return employee
-        }
-        else{
-            throw Exception("Invalid input")
-        }
-    }
-
-    //TODO: Figure out why this function is not working, what exactly is requested here
-    //TODO: we might also need a getFunction to get all employees
-    @MutationMapping
-    fun newEmployee(createEmployeeInput: CreateEmployeeInput): Employee {
-        if(createEmployeeInput.name != null && createEmployeeInput.dateOfBirth != null && createEmployeeInput.city != null && createEmployeeInput.salary != null) {
-            val employee = Employee(
-                // Map the fields from `createEmployeeInput` to `Employee`
-                name = createEmployeeInput.name,
-                dateOfBirth = createEmployeeInput.dateOfBirth,
-                city = createEmployeeInput.city,
-                salary = createEmployeeInput.salary,
-                gender = createEmployeeInput.gender,
-                email = createEmployeeInput.email
-            )
-        return employee
         }
         else{
             throw Exception("Invalid input")
